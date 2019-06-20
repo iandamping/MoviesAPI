07c6cde63211b8e9ae7ae47aa1ff1703ad965577
junaja <jomblangan27@gmail.com>
2019-06-20 16:49:19 +0700

    implement usage of new koin library from antonio leiva course
    https://www.youtube.com/watch?v=fizzD_vCbIw

--- app/src/main/java/com/ian/app/muviepedia/di/ViewModelModule.kt
+++ app/src/main/java/com/ian/app/muviepedia/di/ViewModelModule.kt
@@ -7,6 +7,7 @@ import com.ian.app.muviepedia.data.viewmodel.movie.GetHomeMovieViewModel
 import com.ian.app.muviepedia.data.viewmodel.movie.GetLocalDataViewModel
 import com.ian.app.muviepedia.data.viewmodel.tv.GetDetailTvViewModel
 import com.ian.app.muviepedia.data.viewmodel.tv.GetHomeTvViewModel
+import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 /**
@@ -15,11 +16,11 @@ Created by Ian Damping on 02/06/2019.
 Github = https://github.com/iandamping
  */
 val allVmModule = module {
-    factory { GetHomeMovieViewModel(get()) }
-    factory { GetMoviePagingDataViewModel(get()) }
-    factory { GetDetailMovieViewModel(get()) }
-    factory { GetLocalDataViewModel(get()) }
-    factory { GetHomeTvViewModel(get()) }
-    factory { GetDetailTvViewModel(get()) }
-    factory { GetTvPagingDataViewModel(get()) }
+    viewModel { GetHomeMovieViewModel(get()) }
+    viewModel { GetMoviePagingDataViewModel(get()) }
+    viewModel { GetDetailMovieViewModel(get()) }
+    viewModel { GetLocalDataViewModel(get()) }
+    viewModel { GetHomeTvViewModel(get()) }
+    viewModel { GetDetailTvViewModel(get()) }
+    viewModel { GetTvPagingDataViewModel(get()) }
 }
